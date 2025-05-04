c6ed83acbd5da8dde2a2132bd4447fa27fbfb261
klyde <143183610+23200056@users.noreply.github.com>
2025-05-05 07:18:34 +0800

    ...

---
+++ app/src/main/java/com/example/mindvault/ui/profile/Profile.java
@@ -0,0 +1,18 @@
+package com.example.mindvault.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mindvault.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+}
